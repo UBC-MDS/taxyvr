2a7b1b0fe9b4b33bb79baa96ab4c5e8934592493
hayley <hayleyfboyce@hotmail.com>
2020-03-11 13:51:23 -0700

    2018 data added

---
+++ data-raw/tax_2018.R
@@ -0,0 +1,27 @@
+library(dplyr)
+library(readr)
+
+
+raw <-
+  read_csv2(
+    "https://opendata.vancouver.ca/explore/dataset/property-tax-report/download/?format=csv&refine.report_year=2018&timezone=America/Los_Angeles&lang=en&use_labels_for_header=true&csv_separator=%3B"
+  )
+
+raw %>% head()
+
+tax_2018 <- raw %>%
+  mutate(
+    LEGAL_TYPE = as.factor(LEGAL_TYPE),
+    FOLIO = as.numeric(FOLIO),
+    LAND_COORDINATE = as.numeric(LAND_COORDINATE),
+    ZONE_NAME = as.factor(ZONE_NAME),
+    ZONE_CATEGORY = as.factor(ZONE_CATEGORY),
+    PROPERTY_POSTAL_CODE = as.factor(PROPERTY_POSTAL_CODE),
+    NEIGHBOURHOOD_CODE = as.factor(NEIGHBOURHOOD_CODE)
+  ) %>%
+  rename_all(tolower)
+
+
+
+write_csv(tax_2018, "data-raw/tax_2018.csv")
+save(tax_2018, file = "data/tax_2018.rda", compress='bzip2')
