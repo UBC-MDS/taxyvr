d3fe9ceb526a388c3c775811a193ba31eaa980df
hayley <hayleyfboyce@hotmail.com>
2020-03-17 13:48:56 -0700

    kept just numerics

--- data-raw/tax_2019.R
+++ data-raw/tax_2019.R
@@ -11,16 +11,10 @@ raw %>% head()
 
 tax_2019 <- raw %>%
   mutate(
-    LEGAL_TYPE = as.factor(LEGAL_TYPE),
     FOLIO = as.numeric(FOLIO),
     LAND_COORDINATE = as.numeric(LAND_COORDINATE),
-    ZONE_NAME = as.factor(ZONE_NAME),
-    ZONE_CATEGORY = as.factor(ZONE_CATEGORY),
-    PROPERTY_POSTAL_CODE = as.factor(PROPERTY_POSTAL_CODE),
-    NEIGHBOURHOOD_CODE = as.factor(NEIGHBOURHOOD_CODE)
   ) %>%
   rename_all(tolower)
-
 
 
 write_csv(tax_2019, "data-raw/tax_2019.csv")
